9f4ebc793d0b875fb5e3b495315b623884c1d3eb
monzurece <monzurece@gmail.com>
2024-09-08 14:32:57 +0600

    first example of oop practice

---
+++ course-details.php
@@ -0,0 +1,13 @@
+<?php
+include ('class-lib.php');
+
+
+// Creating instance of the previous class ... Somethink like image/protibimbo/reflection/shadow of class
+$job_placement_course = new Course();
+// $poly_intern_course = new Course();
+
+echo $job_placement_course -> course_name="English";
+
+
+
+?>
