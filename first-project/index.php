534437c3cd8ce322e91aca75ed0165680f677173
monzur rahman <89568953+monzurrahman@users.noreply.github.com>
2024-09-08 16:51:30 +0600

    Call class for difference purposes

--- first-project/index.php
+++ first-project/index.php
@@ -1,1 +1,16 @@
+<?php
+include ('class-lib.php');
 
+
+// Creating instance of the previous class ... Somethink like image/protibimbo/reflection/shadow of class
+$job_placement_course = new Course();
+// $poly_intern_course = new Course();
+
+echo $job_placement_course -> course_name="English";
+echo "<br/>";
+// new object create 
+
+$latest_course = new Course();
+echo $latest_course -> course_name="New course Math";
+
+?>
