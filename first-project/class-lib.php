6babcae4fc852416067a711a08d27e55f47a3053
monzur rahman <89568953+monzurrahman@users.noreply.github.com>
2024-09-09 17:27:00 +0600

    Update class-lib.php

--- first-project/class-lib.php
+++ first-project/class-lib.php
@@ -1,30 +1,24 @@
 <?php
 class Course{
-	// variables defined inside a class is called member variable
-	// Class contains data/property and method/function 
+	var $coursename;
+	var $coursefees;
+	var $courseoutline;
+	var $courseduration;
 	
-	// Property set/get
-	var $course_name="coursename from class file";
-	var $course_fee;
-	var $course_outline;
-	var $course_duration;
-	
-	// functions /methods 
-	
-	function set_course_details($name,$fee,$outline,$duration){
-		$this -> course_name=$name; // Like $course_name = "Math"; can access any property
-		$this -> course_fee=$fee; 
-		$this -> course_outline=$outline;
-		$this -> course_duration=$duration;
-				
+	function set_course_details($name,$fees, $outline, $duration){
+		
+		$this -> coursename=$name;
+		$this -> coursefees=$fees;
+		$this -> courseoutline=$outline;
+		$this -> courseduration=$duration;
+		// return "Your course name:".$name."Course Fees:".$fees."Outline:".$outline."Duration:".$duration;
+		// return $name.$id;
 	}
-	
+
 	function get_course_details(){
-		
-		return "Your course is".$this->course_name." Course fee is:".$this->course_fee." Course outline:".$this->course_outline." Course duration is:".$this->course_duration;
-		
+		return "Your course name:".$this->coursename."Course Fees:".$this->coursefees."Outline:".$this->courseoutline."Duration:".$this->courseduration;
+	}  
+ 	
+	
 	}
-	
-	
-	//$job_placement_course = new Course();
-}
+
